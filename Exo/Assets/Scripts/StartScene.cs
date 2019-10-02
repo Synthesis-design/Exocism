f0b4fe8a97b1bc2d6eed1c8aa6cc5300a32e88b2
Kim-hee-ah <khak1998@naver.com>
2019-10-03 00:59:29 +0900

    #6 로비 틀 제작
    간단히 이런 식으로 로비 제작했음 함. 자세한 기획 추후 제작 예정

---
+++ Exo/Assets/Scripts/StartScene.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnAnyBtnClick()
+    {
+        Menu.SetActive(true);
+        Info.SetActive(false);
+        Title.SetActive(false);
+    }
+    
+    //좀 더 간결하게 코드를 짜는 법은 없을까나
+    public void Stage1()
+    {
+        SceneManager.LoadScene("Stage1");
+        Debug.Log("1번째 스테이지 오픈");
+    }
+    public void Stage2()
+    {
+        SceneManager.LoadScene("Stage2");
+        Debug.Log("2번째 스테이지 오픈");
+    }
+    public void Stage3()
+    {
+        SceneManager.LoadScene("Stage3");
+        Debug.Log("3번째 스테이지 오픈");
+    }
+    public void Stage4()
+    {
+        SceneManager.LoadScene("Stage4");
+        Debug.Log("4번째 스테이지 오픈");
+    }
+    public void Stage5()
+    {
+        SceneManager.LoadScene("Stage5");
+        Debug.Log("5번째 스테이지 오픈");
+    }
+
+    public GameObject Menu;
+    public GameObject Title;
+    public GameObject Info;
+}
