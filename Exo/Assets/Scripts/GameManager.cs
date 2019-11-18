eb42c1ff989226de0a96b9032519b3aa52e238ff
leejy1685 <55981440+leejy1685@users.noreply.github.com>
2019-11-19 00:03:52 +0900

    #8 행동력시스템 제작중

---
+++ Exo/Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private void Start()
+    {
+        System.DateTime nowTime = System.DateTime.Now;
+        Debug.Log(nowTime.Hour);
+        Debug.Log(nowTime.Minute);
+        Debug.Log(nowTime.Second);
+    }
+}
--- Exo/Assets/Scripts/StartScene.cs
+++ Exo/Assets/Scripts/StartScene.cs
@@ -1,21 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class StartScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("희아 선배는 짱짱 멋진 선배님이다!");
-        //양심어디?    
-    }
+    public Text Stemina;
+    public float time=0.0f;
+    public float SteminaNum=0.0f; 
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if(SteminaNum<5)
+        {
+            time += Time.deltaTime;
 
+            if (time > 5.0f && time < 6.0f)
+            {
+                SteminaNum++;
+
+                Stemina.text = SteminaNum.ToString() + "/5";
+                time = 0.0f;
+            }
+        }
+        
     }
 
     public void OnAnyBtnClick()
@@ -28,28 +38,58 @@ public class StartScene : MonoBehaviour
     //좀 더 간결하게 코드를 짜는 법은 없을까나
     public void Stage1()
     {
-        SceneManager.LoadScene("Stage1");
-        Debug.Log("1번째 스테이지 오픈");
+        if (SteminaNum == 0)
+            Debug.Log("불가능");
+        else
+        {
+            SceneManager.LoadScene("Stage1");
+            Debug.Log("1번째 스테이지 오픈");
+            SteminaNum--;
+        }
     }
     public void Stage2()
     {
-        SceneManager.LoadScene("Stage2");
-        Debug.Log("2번째 스테이지 오픈");
+        if (SteminaNum == 0)
+            Debug.Log("불가능");
+        else
+        {
+            SceneManager.LoadScene("Stage2");
+            Debug.Log("2번째 스테이지 오픈");
+            SteminaNum--;
+        }
     }
     public void Stage3()
     {
-        SceneManager.LoadScene("Stage3");
-        Debug.Log("3번째 스테이지 오픈");
+        if (SteminaNum == 0)
+            Debug.Log("불가능");
+        else
+        {
+            SceneManager.LoadScene("Stage3");
+            Debug.Log("3번째 스테이지 오픈");
+            SteminaNum--;
+        }
     }
     public void Stage4()
     {
-        SceneManager.LoadScene("Stage4");
-        Debug.Log("4번째 스테이지 오픈");
+        if (SteminaNum == 0)
+            Debug.Log("불가능");
+        else
+        {
+            SceneManager.LoadScene("Stage4");
+            Debug.Log("4번째 스테이지 오픈");
+            SteminaNum--;
+        }
     }
     public void Stage5()
     {
-        SceneManager.LoadScene("Stage5");
-        Debug.Log("5번째 스테이지 오픈");
+        if (SteminaNum == 0)
+            Debug.Log("불가능");
+        else
+        {
+            SceneManager.LoadScene("Stage5");
+            Debug.Log("5번째 스테이지 오픈");
+            SteminaNum--;
+        }
     }
 
     public GameObject Menu;
