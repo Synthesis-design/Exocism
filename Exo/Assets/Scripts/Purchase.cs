98fa132119421bb383e6c74f0b28b15b91379a93
Kim-hee-ah <khak1998@naver.com>
2019-11-18 22:01:44 +0900

    #8 캐릭터 필드 내 생성
    캐릭터 버튼을 클릭하면 필드 내 생성하게 만들었습니다. 이제.. 캐릭터 클릭 시 edit모드로 들어가서 드래그 앤 드롭 기능을 할 수 있도록 할 것입니다...

---
+++ Exo/Assets/Scripts/Purchase.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Purchase : MonoBehaviour
+{
+    GameObject buyPrefab;
+    public Transform gridPos;
+    int count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void getKnight()
+    {
+        buyPrefab = Resources.Load<GameObject>("knight");
+        
+        GameObject player = Instantiate(buyPrefab);
+
+        RectTransform playerPos = player.GetComponent<RectTransform>();
+
+        player.transform.position = gameObject.transform.position;
+    }
+
+    public void getWizard()
+    {
+        buyPrefab = Resources.Load<GameObject>("mage_dark");
+
+        GameObject player = Instantiate(buyPrefab);
+
+        RectTransform playerPos = player.GetComponent<RectTransform>();
+
+        player.transform.position = gameObject.transform.position;
+
+    }
+
+    public void getArcher()
+    {
+        buyPrefab = Resources.Load<GameObject>("archer2");
+
+        GameObject player = Instantiate(buyPrefab);
+
+        RectTransform playerPos = player.GetComponent<RectTransform>();
+
+        player.transform.position = gameObject.transform.position;
+
+    }
+
+
+}
