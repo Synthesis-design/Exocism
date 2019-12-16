4277a1ac34692be7c7e8c93655eabefb6db27705
Kim-hee-ah <khak1998@naver.com>
2019-12-16 22:38:57 +0900

    #8 스타트 버튼 클릭 시 상점 창 사라지게
    대신 아부 멘트가 뜨게 했음 ^^

--- Exo/Assets/Scripts/StartGame.cs
+++ Exo/Assets/Scripts/StartGame.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartGame : MonoBehaviour
 {
+    public GameObject abu;
+    public GameObject pur;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void activePanel()
+    {
+        abu = GameObject.Find("Canvas").transform.Find("InGame").gameObject;
+        pur = GameObject.Find("Purchase").gameObject;
+        abu.gameObject.SetActive(true);
+        pur.gameObject.SetActive(false);
     }
 }
