08bcc094664e1a9d822e9e0216aed1ebac2c76bc
Kim-hee-ah <khak1998@naver.com>
2019-12-17 18:58:20 +0900

    #8 pause 기능 구현

--- Exo/Assets/Scripts/GameManager.cs
+++ Exo/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -34,4 +35,25 @@ public class GameManager : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void OnClickPause()
+    {
+        Time.timeScale = 0;
+
+        Menu.SetActive(true);
+    }
+
+    public void OnClickAgain()
+    {
+        Time.timeScale = 1;
+
+        Menu.SetActive(false);
+    }
+
+    public void OnClickReturn()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+
+    public GameObject Menu;
 }
